b8e09c1d89309b850716789fa7a5eb703ba8063a
Bryan Roof <broof716@gmail.com>
2021-03-22 18:25:25 -0700

    finishing touches

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,11 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
-    <Button 
-      @btn-click="$emit('toggle-add-task')" 
-      :text="showAddTask ? 'Close': 'Add Task'" 
-      :color="showAddTask ? 'red' : 'green'" 
+    <Button
+      v-show="homePage"
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
     />
   </header>
 </template>
@@ -18,16 +19,25 @@ export default {
     showAddTask: Boolean,
   },
   components: {
-    Button
-  }
+    Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === '/') {
+        return true
+      } else {
+        return false
+      }
+    },
+  },
 }
 </script>
 
 <style scoped>
-  header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
-  }
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
 </style>
